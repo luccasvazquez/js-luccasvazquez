097b6ae6fa97ed6f77581c50b698638447661af3
luccasvazquez <lucasvazquez152@gmail.com>
2022-10-05 18:22:56 -0300

    pimer entrega

---
+++ js/simulador.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+function solicitar_usuario () {
+    
+    
+    let usuario = prompt("Ingrese su nombre de usuario");
+    let password = prompt("Ingrese su contraseña");
+
+    if (usuario != "" && password != "") {
+            
+            alert("Bienvenido/a a nuestro sitio " + usuario);
+            
+    }
+    else {
+
+            alert("Error: Usuario y/o contraseña no ingresado");
+    
+    }
+
+}
+
+solicitar_usuario();
+
+console.log("Bienvenido/a a nuestro sitio " + usuario)
